cad6a933bf0aa746c038d10c1d2d142595586b7a
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-25 10:54:14 -0400

    Add actions

--- src/redux/actors.js
+++ src/redux/actors.js
@@ -1,4 +1,6 @@
 export const ADD_ACTOR = 'ADD_ACTOR';
+export const UPDATE_ACTOR = 'UPDATE_ACTOR';
+export const REMOVE_ACTOR = 'REMOVE_ACTOR';
 
 export const addActor = (id, name, bio) => ({
   type: ADD_ACTOR,
@@ -6,5 +8,21 @@ export const addActor = (id, name, bio) => ({
     id,
     name,
     bio,
+  },
+});
+
+export const updateActor = (id, name, bio) => ({
+  type: UPDATE_ACTOR,
+  data: {
+    id,
+    name,
+    bio,
+  },
+});
+
+export const removeActor = id => ({
+  type: REMOVE_ACTOR,
+  data: {
+    id,
   },
 });
--- src/redux/actors.spec.js
+++ src/redux/actors.spec.js
@@ -1,4 +1,4 @@
-import { ADD_ACTOR, addActor } from './actors';
+import { ADD_ACTOR, addActor, REMOVE_ACTOR, removeActor, UPDATE_ACTOR, updateActor } from './actors';
 
 describe('Actors', () => {
   it('ACTION: Add', () => {
@@ -9,6 +9,28 @@ describe('Actors', () => {
         id: 1,
         name: 'Someone',
         bio: 'Born somewhere on 1944',
+      },
+    });
+  });
+
+  it('ACTION: Update', () => {
+    const action = updateActor(1, 'Someone', 'Born somewhere on 1944');
+    expect(action).toEqual({
+      type: UPDATE_ACTOR,
+      data: {
+        id: 1,
+        name: 'Someone',
+        bio: 'Born somewhere on 1944',
+      },
+    });
+  });
+
+  it('ACTION: Remove', () => {
+    const action = removeActor(1);
+    expect(action).toEqual({
+      type: REMOVE_ACTOR,
+      data: {
+        id: 1,
       },
     });
   });
