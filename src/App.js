724623caf6ba9d247fb34c2fd94ed29beb060225
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-25 17:55:35 -0400

    Add navigation to root component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Navigation from './components/navigation/navigation';
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <Router>
+        <div className="main">
+          <Route path="/">
+            <Navigation />
+          </Route>
+        </div>
+      </Router>
     );
   }
 }
