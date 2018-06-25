ce94d1024e7b3cdfc6e7cf382676e2ef6e2548db
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-25 18:45:24 -0400

    Provide store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './App.css';
 import Navigation from './components/navigation/navigation';
 import MovieList from './components/movie-list/movie-list';
@@ -7,16 +8,18 @@ import MovieList from './components/movie-list/movie-list';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="main">
-          <Route path="/">
-            <Navigation />
-          </Route>
-          <Route
-              path="/movies"
-              component={MovieList} />
-        </div>
-      </Router>
+      <Provider store={this.props.store}>
+        <Router>
+          <div className="main">
+            <Route
+                path="/"
+                component={Navigation} />
+            <Route
+                path="/movies"
+                component={MovieList} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
