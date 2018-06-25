a983ae8fc7f94ec250cdd691d13ed59e93a27260
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-25 15:34:01 -0400

    Provide mock actions

--- src/components/actor-input/actor-input.stories.js
+++ src/components/actor-input/actor-input.stories.js
@@ -1,11 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies,global-require */
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { storiesOf, action } from '@storybook/react';
 import ActorInput from './actor-input';
 
 storiesOf('Actor Input', module)
   .add('Default', () => (
     <ActorInput
         name="Some Guy"
-        bio="Bio text" />
+        bio="Bio text"
+        actions={{
+          create: action(),
+          update: action(),
+        }} />
   ));
