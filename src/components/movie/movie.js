a586981473e468b6e9108a05aed9bc60caeda429
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-25 10:25:57 -0400

    Use title component

--- src/components/movie/movie.js
+++ src/components/movie/movie.js
@@ -3,16 +3,13 @@ import PropTypes from 'prop-types';
 import Card from '@material-ui/core/es/Card/Card';
 import Typography from '@material-ui/core/es/Typography/Typography';
 import Chip from '@material-ui/core/es/Chip/Chip';
+import Title from '../title/title';
 
 const Movie = ({
   title, synopsis, genres, cast,
 }) => (
   <Card>
-    <Typography
-        variant="title"
-        gutterBottom>
-      {title}
-    </Typography>
+    <Title value={title} />
     {genres.map(genre => (
       <Chip
           key={`genre-${genre}`}
