d14a8a7ae615fd5950c1c82bbcf8d10c89f77f8c
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-26 18:29:02 -0400

    List genres and their movies from store

--- src/components/genre-list/genre-list.js
+++ src/components/genre-list/genre-list.js
@@ -5,13 +5,15 @@ import { connect } from 'react-redux';
 
 export const GenreList = ({ genres }) => (
   Object.entries(genres).map(([genre, movies]) => (
-    <Fragment key={`genre-item-${genre}`}>
+    <div key={`genre-item-${genre}`}>
       <h4>{genre}</h4>
-      {movies.map(({ id, title }) => (
-        <p key={`/movie-item-${id}`}>
-          <Link to={`/movies/${id}`}>{title}</Link>
-        </p>))}
-    </Fragment>
+      <ul>
+        {movies.map(({ id, title }) => (
+          <li key={`/movie-item-${id}`}>
+            <Link to={`/movies/${id}`}>{title}</Link>
+          </li>))}
+      </ul>
+    </div>
   ))
 );
 
@@ -19,6 +21,18 @@ GenreList.propTypes = {
   genres: PropTypes.object,
 };
 
-const mapStateToProps = ({ movies }) => ({});
+const mapStateToProps = ({ movies }) => ({
+  genres: Object.entries(movies)
+    .reduce((all, [id, movie]) => [
+      ...all,
+      ...movie.genres.filter(genre => !all.includes(genre)),
+    ], [])
+    .reduce((all, genre) =>
+      Object.assign({}, all, {
+        [genre]: Object.entries(movies)
+          .filter(([_, movie]) => movie.genres.includes(genre))
+          .map(([id, movie]) => Object.assign({ id }, movie)),
+      }), {}),
+});
 
 export default connect(mapStateToProps)(GenreList);
