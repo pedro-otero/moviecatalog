1fbd9e5d5048b4d7748da2bd3cca59b0275e9498
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-27 18:57:13 -0400

    Change layout

--- src/components/actor-list/actor-list.js
+++ src/components/actor-list/actor-list.js
@@ -1,33 +1,30 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import Actor from '../actor/actor';
 import DeleteButton from '../delete-button/delete-button';
 import { removeActor } from '../../redux/actors';
 import AddButton from '../add-button/add-button';
 
 export const ActorList = ({ actors, remove }) => (
-  <ul className="list-group">
-    {actors.map(({ id, name }) => (
-      <li
-          className="list-group-item"
-          key={`list-actor-${name}`}>
-        <div className="container">
-          <div className="row">
-            <Link
-                to={`/actors/${id}`}
-                className="col col-11">{name}</Link>
-            <DeleteButton
-                className="col col-1"
-                action={() => remove(id)} />
-          </div>
+  <Fragment>
+    <div className="container">
+      {actors.map(({ id, name }) => (
+        <div
+            className="row pb-2 pt-2 border-bottom"
+            key={`list-actor-${name}`}>
+          <Link
+              to={`/actors/${id}`}
+              className="col col-11">{name}</Link>
+          <DeleteButton
+              className="col col-1"
+              action={() => remove(id)} />
         </div>
-      </li>
       ))}
+    </div>
     <br />
     <AddButton path="/add/actor" />
-  </ul>
+  </Fragment>
 );
 
 ActorList.propTypes = {
