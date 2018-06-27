c5da369e74ea364fa016cea8f1bd1a0d0352348c
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-27 15:26:02 -0400

    Use mock state

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,18 @@
 import { createStore, combineReducers } from 'redux';
 import { reduceActors } from './actors';
 import { reduceMovies } from './movies';
+import mockState from '../mock/state.json';
 
 const devTools = global.window.__REDUX_DEVTOOLS_EXTENSION__ &&
   global.window.__REDUX_DEVTOOLS_EXTENSION__();
 
 export default function () {
+  if (process.env.REACT_APP_MOCK === 'YES') {
+    return createStore(combineReducers({
+      actors: reduceActors,
+      movies: reduceMovies,
+    }), mockState, devTools);
+  }
   return createStore(combineReducers({
     actors: reduceActors,
     movies: reduceMovies,
