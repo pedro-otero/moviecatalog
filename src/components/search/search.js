fcab1aaf53260a5be2d6748b739b46b542d1c1a1
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-27 11:10:10 -0400

    Add search stories

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Search extends React.Component {
   static getDerivedStateFromProps({ filter, movies }) {
@@ -16,10 +17,32 @@ class Search extends React.Component {
     };
   }
 
+  renderMovies(header, movies) {
+    return <Fragment>
+      <h4>{header}</h4>
+      <ul className="list-group">
+        {movies.map(({ id, title }) => (
+          <li
+              className="list-group-item"
+              key={`list-movie-${title}`}>
+            <Link to={`/movies/${id}`}>{title}</Link>
+          </li>
+        ))}
+      </ul>
+    </Fragment>;
+  }
+
   render() {
+    const { filter } = this.props;
+    const {
+      byTitle, bySynopsis, byGenre, byCast,
+    } = this.state;
     return (
       <div>
-
+        {byTitle.length > 0 && this.renderMovies(`Titles containing "${filter}"`, byTitle)}
+        {bySynopsis.length > 0 && this.renderMovies(`About "${filter}"`, bySynopsis)}
+        {byGenre.length > 0 && this.renderMovies(`Genre: "${filter}"`, byGenre)}
+        {byCast.length > 0 && this.renderMovies(`Starring "${filter}"`, byCast)}
       </div>
     );
   }
---
+++ src/components/search/search.stories.js
@@ -0,0 +1,48 @@
+/* eslint-disable import/no-extraneous-dependencies,global-require */
+import React from 'react';
+import { storiesOf, action } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
+import Search from './search';
+
+const movies = [{
+  id: 'anId',
+  title: 'A Funny Movie',
+  synopsis: 'This one makes you laugh',
+  cast: ['AC1', 'AC2'],
+  genres: ['Comedy'],
+}, {
+  title: 'Slashing And Blood',
+  synopsis: 'This one will give you nightmares',
+  cast: ['AC2', 'AC3', 'AC4'],
+  genres: ['Horror', 'Thriller'],
+}, {
+  title: 'Super-Hyped film',
+  synopsis: 'Everyone talks about this one',
+  cast: ['Meryl Streep'],
+  genres: ['Thriller'],
+}];
+
+storiesOf('Search', module)
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+  ))
+  .add('Title', () => (
+    <Search
+        filter="Movie"
+        movies={movies} />
+  ))
+  .add('Synopsis', () => (
+    <Search
+        filter="nightmares"
+        movies={movies} />
+  ))
+  .add('Genre', () => (
+    <Search
+        filter="thriller"
+        movies={movies} />
+  ))
+  .add('Cast', () => (
+    <Search
+        filter="meryl streep"
+        movies={movies} />
+  ));
