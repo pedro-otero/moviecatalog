676927e28b98cba5fb666bc95ba333dccc898cf1
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-27 14:11:11 -0400

    Remove unused import

--- src/components/search-input/search-input.js
+++ src/components/search-input/search-input.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Redirect, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 class SearchInput extends React.Component {
   state = {
