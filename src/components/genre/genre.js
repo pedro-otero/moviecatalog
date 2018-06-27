3114613e97b60918b9d51f9a742845e57d809776
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-27 14:02:07 -0400

    Restore delete genres functionality

--- src/components/genre/genre.js
+++ src/components/genre/genre.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Genre = ({ name }) => (
-  <span className="badge badge-pill badge-info">{name}</span>
+const Genre = ({ name, onClick }) => (
+  <span
+      className="badge badge-pill badge-info"
+      onClick={onClick}>
+    {name}
+  </span>
 );
 
 Genre.propTypes = {
   name: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
 };
 
 export default Genre;
