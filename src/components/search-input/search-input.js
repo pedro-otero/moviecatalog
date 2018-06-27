c5d246652fb8f331c9b39cad24a8b04f24d558c4
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-27 11:20:16 -0400

    Add search input component

---
+++ src/components/search-input/search-input.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+class SearchInput extends React.Component {
+  onFilterChange = (e) => {
+    this.setState({ filter: e.target.value });
+  };
+
+  render() {
+    const { filter } = this.state;
+    return <form>
+      {filter && <Redirect to={`/search?filter=${filter}`} />}
+      <div className="form-group">
+        <input
+            id="search-filter"
+            className="form-control"
+            type="text"
+            value={filter}
+            onChange={this.onFilterChange} />
+      </div>
+    </form>;
+  }
+}
+
+export default SearchInput;
