b3e60d185342e167569b4d340487beb215f99217
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-27 17:24:25 -0400

    List actor movies

--- src/components/actor/actor.js
+++ src/components/actor/actor.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import EditButton from '../edit-button/edit-button';
 
-export const Actor = ({ id, name, bio }) => (
+export const Actor = ({
+  id, name, bio, movies,
+}) => (
   <div>
     <h2>{name}</h2>
     <p>{bio}</p>
+    {movies.length > 0 && <Fragment>
+      <p><strong>Movies:</strong></p>
+      {movies.map(({ movieId, title }) => (
+        <p key={`movie-item-${movieId}`}>
+          <Link to={`/movies/${movieId}`}>{title}</Link>
+        </p>
+      ))}
+    </Fragment>}
     <EditButton path={`/edit/actor/${id}`} />
   </div>
 );
@@ -14,12 +25,16 @@ export const Actor = ({ id, name, bio }) => (
 Actor.propTypes = {
   bio: PropTypes.string,
   id: PropTypes.string,
+  movies: PropTypes.array,
   name: PropTypes.string.isRequired,
 };
 
-const mapStateToProps = ({ actors }, { id }) => ({
+const mapStateToProps = ({ actors, movies }, { id }) => ({
   name: actors[id].name,
   bio: actors[id].bio,
+  movies: Object.entries(movies)
+    .filter(([_, { cast }]) => cast.includes(id))
+    .map(([movieId, { title }]) => ({ movieId, title })),
 });
 
 export default connect(mapStateToProps)(Actor);
