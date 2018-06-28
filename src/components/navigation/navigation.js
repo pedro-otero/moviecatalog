0649cb802f2764ce256e3f16e12c45461339c3e4
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-27 23:21:09 -0400

    Swap

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Navigation = () => (
   <div className="d-flex flex-row flex-md-column justify-content-center justify-content-md-start">
     <p className="pr-3 pr-md-0"><Link to="/movies">Movies</Link></p>
-    <p className="pr-3 pr-md-0"><Link to="/genres">Genres</Link></p>
     <p className="pr-3 pr-md-0"><Link to="/actors">Actors</Link></p>
+    <p className="pr-3 pr-md-0"><Link to="/genres">Genres</Link></p>
   </div>
 );
 
