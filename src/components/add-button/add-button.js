6c166eaaf3e4eb496b79386546247b9861c7c813
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-27 23:42:58 -0400

    Code structure

--- src/components/add-button/add-button.js
+++ src/components/add-button/add-button.js
@@ -2,13 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const AddButton = ({ path }) => (
-  <Link
-      to={path}
-      className="btn btn-primary">
-      Add
-    </Link>
-);
+const AddButton = ({ path }) => <Link to={path} className="btn btn-primary">Add</Link>;
 
 AddButton.propTypes = {
   path: PropTypes.string.isRequired,
