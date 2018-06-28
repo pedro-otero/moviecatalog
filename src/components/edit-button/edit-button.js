90b645217d526d1fd696a8ecb9a4dbb9268fcd5f
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-27 23:43:40 -0400

    Code structure

--- src/components/edit-button/edit-button.js
+++ src/components/edit-button/edit-button.js
@@ -2,13 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const EditButton = ({ path }) => (
-  <Link
-      to={path}
-      className="btn btn-primary">
-      Edit
-    </Link>
-);
+const EditButton = ({ path }) => <Link to={path} className="btn btn-primary">Edit</Link>;
 
 EditButton.propTypes = {
   path: PropTypes.string.isRequired,
