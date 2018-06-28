d2cdce8f415c146ac6d58953ecb7764dee41dab6
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-28 00:22:11 -0400

    Refactor

--- src/components/search-input/search-input.js
+++ src/components/search-input/search-input.js
@@ -3,33 +3,22 @@ import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
 class SearchInput extends React.Component {
-  state = {
-    filter: '',
-  };
-
-  onFilterChange = (e) => {
-    this.setState({ filter: e.target.value });
-  };
-
   enterSearch = (e) => {
     if (e.keyCode === 13) {
       e.preventDefault();
-      this.setState({ filter: '' });
-      this.props.history.push(`/search?filter=${this.state.filter}`);
+      this.props.history.push(`/search?filter=${e.target.value}`);
+      e.target.value = '';
     }
   };
 
   render() {
-    const { filter } = this.state;
     return <form>
       <div className="form-group">
         <input
             id="search-filter"
             className="form-control"
             type="text"
-            value={filter}
             placeholder="Search..."
-            onChange={this.onFilterChange}
             onKeyDown={this.enterSearch} />
       </div>
     </form>;
